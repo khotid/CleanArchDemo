7a5c83cffbda3b4558b43d2a1cb69a49327c30af
Indrajeet <khotid@gmail.com>
2020-01-19 13:49:09 +0530

    Added course service and repository

---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,21 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDbContext _ctx;
+        public CourseRepository(UniversityDbContext ctx) {
+            _ctx = ctx;
+        }
+        IEnumerable<Course> ICourseRepository.GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
